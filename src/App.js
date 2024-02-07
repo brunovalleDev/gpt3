2217759ca567fc814e95d29abf938f3956a166ae
Bruno Valle Silva <brunovallesilva93@gmail.com>
2024-02-07 17:31:29 -0300

    Hero Section finalizada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Navbar } from './components';
+import { Navbar} from './components';
+import {Hero} from './container/index';
 import './App.css'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className="App">
       <div className="gradient__bg">
         <Navbar />
+        <Hero />
       </div>
     </div>
   )
